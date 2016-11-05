2c6219700c61053fa52309d3d96dc61d03584543
Zhentar <Zhentar@gmail.com>
2016-11-05 15:28:34 -0500

    Add project files.

---
+++ Source/CompAutoFlicker.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace AutoFlicker
+{
+    public class CompAutoFlicker : ThingComp
+    {
+
+        private bool enabled = false;
+
+        private int currDaysPassed;
+
+        private bool hasFlickedOnToday;
+
+        private bool hasFlickedOffToday;
+
+
+        private CompFlickable flickableComp;
+
+        public CompProperties_AutoFlicker Props => (CompProperties_AutoFlicker)this.props;
+
+	    public override void PostSpawnSetup()
+        {
+            base.PostSpawnSetup();
+            this.flickableComp = this.parent.GetComp<CompFlickable>();
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+
+            CheckDesignateFlick();
+        }
+
+        //Make it compatible with both ticker types
+        public override void CompTickRare()
+        {
+            base.CompTickRare();
+
+            CheckDesignateFlick();
+        }
+
+        private void CheckDesignateFlick()
+        {
+            if (!enabled) { return; }
+
+            if (GenDate.DaysPassed > this.currDaysPassed)
+            {
+                hasFlickedOnToday = false;
+                hasFlickedOffToday = false;
+                this.currDaysPassed = GenDate.DaysPassed;
+            }
+
+            bool? wantsFlickOn = null;
+
+            if (!hasFlickedOnToday && GenDate.CurrentDayPercent > this.Props.flickOnPercent)
+            {
+                wantsFlickOn = true;
+                hasFlickedOnToday = true;
+            }
+            if (!hasFlickedOffToday && GenDate.CurrentDayPercent > this.Props.flickOffPercent)
+            {
+                wantsFlickOn = false;
+                hasFlickedOffToday = true;
+            }
+
+            if (wantsFlickOn == null)
+            {
+                return;
+            }
+
+
+            var curState = flickableComp.SwitchIsOn;
+            if (flickableComp.WantsFlick())
+            {
+                curState = !curState;
+            }
+
+            if (curState != wantsFlickOn)
+            {
+                var field = typeof(CompFlickable).GetField("wantSwitchOn", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
+                field.SetValue(flickableComp, wantsFlickOn);
+                FlickUtility.UpdateFlickDesignation(this.parent);
+            }
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.LookValue(ref this.enabled, nameof(this.enabled), false);
+            Scribe_Values.LookValue(ref this.currDaysPassed, nameof(this.currDaysPassed), 0);
+            Scribe_Values.LookValue(ref this.hasFlickedOnToday, nameof(this.hasFlickedOnToday), false);
+            Scribe_Values.LookValue(ref this.hasFlickedOffToday, nameof(this.hasFlickedOffToday), false);
+        }
+
+        public override IEnumerable<Command> CompGetGizmosExtra()
+        {
+            foreach (var c in base.CompGetGizmosExtra())
+            {
+                yield return c;
+            }
+            if (this.parent.Faction == Faction.OfPlayer)
+            {
+                yield return new Command_Toggle
+                {
+                    hotKey = KeyBindingDefOf.CommandItemForbid,
+                    icon = TexCommand.Forbidden,
+                    defaultLabel = "Auto Flicking",
+                    defaultDesc = "Automatically instructs Pawns to flick on during the day and off at night",
+                    isActive = () => this.enabled,
+                    toggleAction = () => this.enabled = !this.enabled
+                };
+            }
+        }
+    }
+}
---
+++ Source/CompProperties_AutoFlicker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace AutoFlicker
+{
+	public class CompProperties_AutoFlicker : CompProperties
+	{
+
+        public float flickOnPercent = 0.23f; /* not 0.25 to make pawns plan ahead */
+
+        public float flickOffPercent = 0.8f;
+
+        public CompProperties_AutoFlicker()
+		{
+			this.compClass = typeof(CompAutoFlicker);
+		}
+	}
+}
